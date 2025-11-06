1528ad64449b9a9b55285c19365843f48f73ad3d
Kan1es <monster_games32@mail.ru>
2025-11-06 20:44:37 +0400

    Translate to Russian

--- cogs/uinfo.py
+++ cogs/uinfo.py
@@ -9,17 +9,17 @@ class UserInfo(commands.Cog):
     @commands.command()
     async def uinfo(self, ctx):
         em = discord.Embed(
-            title="**👤 User information**",
+            title="**👤 Информация о пользователе**",
             color=discord.Color.blurple()
         )
         user = ctx.author
         ws_ms = int(self.bot.latency * 1000)
-        em.add_field(name="Username:", value=user.mention, inline=True)
+        em.add_field(name="Имя:", value=user.mention, inline=True)
         em.add_field(name="ID:", value=user.id, inline=True)
-        em.add_field(name="Ping:", value=f"{ws_ms} ms", inline=True)
-        em.add_field(name="Joined Server at:", value=user.joined_at.strftime("%Y-%m-%d %H:%M:%S"), inline=True)
-        em.add_field(name="Joined Discord at:", value=user.created_at.strftime("%Y-%m-%d %H:%M:%S"), inline=True)
-        em.add_field(name="Status:", value=user.status, inline=True)
+        em.add_field(name="Пинг:", value=f"{ws_ms} ms", inline=True)
+        em.add_field(name="Присоединился к серверу:", value=user.joined_at.strftime("%Y-%m-%d %H:%M:%S"), inline=True)
+        em.add_field(name="Создал дискорд:", value=user.created_at.strftime("%Y-%m-%d %H:%M:%S"), inline=True)
+        em.add_field(name="Статус:", value=user.status, inline=True)
         await ctx.send(embed=em)
 
 
